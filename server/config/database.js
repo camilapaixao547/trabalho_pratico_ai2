ba6fce8afd17e90813279a52b83459d27f1d4fa9
martinsdiana93 <martinsdiana93@hotmail.com>
2026-06-20 00:19:17 +0100

    database

--- server/config/database.js
+++ server/config/database.js
@@ -1,4 +1,4 @@
-const { Sequelize } = require('sequelize')
+/* const { Sequelize } = require('sequelize')
 
 const sequelize = new Sequelize(
     'pantureco_db',
@@ -9,5 +9,19 @@ const sequelize = new Sequelize(
         dialect: 'postgres'
     }
 )
+
+module.exports = sequelize */
+
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+})
 
 module.exports = sequelize
