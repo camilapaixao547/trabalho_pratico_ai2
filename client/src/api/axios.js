a0e3d656cdf458fd16d3c35e6a2cf50952a384e2
martinsdiana93 <martinsdiana93@hotmail.com>
2026-06-21 23:46:15 +0100

    axios fix

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,16 @@
 import axios from 'axios'
 
-const api = axios.create({
+/* const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+}) */
+
+const api = axios.create({
+    baseURL: window.location.hostname === 'localhost'
+        ? 'http://localhost:5000/api'
+        : '/api',
     headers: {
         'Content-Type': 'application/json',
     },
